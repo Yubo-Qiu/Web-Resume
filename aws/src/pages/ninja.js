1ff0d178069557a9efbfa1a1386ac063262169eb
Yubo-Qiu <yubo@bu.edu>
2024-01-10 03:22:54 -0500

    ninja half done

--- aws/src/pages/ninja.js
+++ aws/src/pages/ninja.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import './ninja.css'; // Importing the CSS file for styles
-import wav from '../images/monica.jpg';
+import planner from '../images/planner.png';
+import error from '../images/error.jpg';
 
 function Ninja() {
     return (
         <div className="rectangle-container">
-            <img src={wav} alt="Ninja" className="rectangle-image" />
+            <img src={planner} alt="Ninja" className="rectangle-image" />
             <h1>Course Auto-Register</h1>
             <bullet>Description:</bullet>
-            <c>This is where your content will go. You can add more content elements inside this container as needed.</c>
-            <c>This is where your content will go. You can add more content elements inside this container as needed.</c>
-            <c>This is where your content will go. You can add more content elements inside this container as needed.</c>
-            <bullet>This:</bullet>
-            <c>This is where your content will go. You can add more content elements inside this container as needed.</c>
-            <c>This is where your content will go. You can add more content elements inside this container as needed.</c>
-            <img src={wav} alt="Jarvis" className="rectangle-image2" />
-            <c>This is where your content will go. You can add more content elements inside this container as needed.</c>
-            <c>This is where your content will go. You can add more content elements inside this container as needed.</c>
-            <bullet>This:</bullet>
-            <c>This is where your content will go. You can add more content elements inside this container as needed.</c>
-            <c>This is where your content will go. You can add more content elements inside this container as needed.</c>
-            <img src={wav} alt="Jarvis" className="rectangle-image2" />
-            <c>This is where your content will go. You can add more content elements inside this container as needed.</c>
+            <c>This project features a <b>Python script</b> that performs preprogrammed click actions on the BU Student Link course registration website. This functionality is achieved using the <b>Selenium</b> library.</c>
+            <c>It is adept at quickly registering for all courses in the Planner as soon as registration opens. Additionally,  
+                the script can automatically refresh the registration page and promptly enroll in a specific course the moment an available seat is detected.</c>
+            <bullet>Declaration:</bullet>
+            <c><b>This project was developed solely for research purposes.</b> It has never been utilized, nor will it be used in the future, to gain unfair advantage 
+                or to outcompete other students in course registration.</c>
+            <bullet>Specifics:</bullet>
+            <bullet>Shush!</bullet>
+            <img src={error} alt="Jarvis" className="rectangle-image-ninja2" />
         </div>
     );
 }
--- aws/src/test.js
+++ aws/src/test.js
@@ -3,7 +3,8 @@ import React from 'react';
 function Test() {
   return (
     <div>
-      <h1 style={{color: 'black'}}>This is a separate page (the Test component)</h1>
+      <h1 style={{color: 'black'}}>Additional details forthcoming.</h1>
+      <c>I am working on it :)</c>
     </div>
   );
 }
